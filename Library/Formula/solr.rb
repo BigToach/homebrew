29fc7eb46a6dbd8bb89d5e1a954c4b6662054ccd
BigToach <roach.scott@gmail.com>
2013-01-25 12:37:19 -0800

    Solr updated to 4.1.0. All of the 4.0.0 mirrors are down. Note the 4.1.0-BETA is not in the mirrors yet and the 4.0.0-BETA mirrors are down as well

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.0.0/apache-solr-4.0.0.tgz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.1.0/solr-4.1.0.tgz'
   sha1 '0cb61d9572516fc627785201b79b3a85e95f877d'
 
   devel do
     url  'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.0.0-BETA/apache-solr-4.0.0-BETA.tgz'
     sha1 'b41061400f3c5e0433ae8e01c4a62814be37b712'
-    version "4.0.0-BETA"
+    version "4.1.0-BETA"
   end
 
   def script; <<-EOS.undent
